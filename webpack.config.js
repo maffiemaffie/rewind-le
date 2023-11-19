ccdb636a33171e84db6fa12977ea31e5984b7d4b
Elia Cohen <mrc6113@rit.edu>
2023-11-19 16:47:35 -0500

    leaving the old ones in until theyre replaced

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 module.exports = {
     entry: {
-        // app: './client/maker.jsx',
-        // search: './client/search.jsx',
-        // login: './client/login.jsx',
+        app: './client/maker.jsx',
+        search: './client/search.jsx',
+        login: './client/login.jsx',
     },
     module: {
         rules: [
