61cb536833e869b082461c5a098f97b5f0d55f0a
Elia Cohen <mrc6113@rit.edu>
2023-12-09 13:44:23 -0500

    all time styling

--- client/stats.jsx
+++ client/stats.jsx
@@ -4,24 +4,70 @@ const ReactDOM = require('react-dom');
 
 const AllTimeStats = (props) => {
     const breakdown = props.allTime.breakdown.map((category) => {
+        const frequencyPercentage = category.frequency * 100 / props.allTime.wins;
+        
         return <li 
         className='breakdownCategory' 
         data-guesses={category.guesses} 
         data-frequency={category.frequency}>
-            {category.frequency}
+            <svg 
+                className='frequencyBar'
+                width='100%' 
+                height='100%' 
+                xmlns='http://www.w3.org/2000/svg'>
+                <rect 
+                    x={0}
+                    y={0}
+                    width={`${frequencyPercentage}%`} 
+                    height='100%'
+                ></rect>
+            </svg>
+            <span className='frequencyValue'>{category.frequency}</span>
     </li>
     });
 
     const rate = Math.floor(props.allTime.wins * 100 / (props.allTime.losses + props.allTime.wins));
 
+    const arcFromRate = (rate) => {
+        const radius = 50;
+
+        const startX = radius * 2;
+        const startY = radius;
+        
+        const angle = Math.min(rate * 0.01 * 2 * Math.PI, 2 * Math.PI - 0.0001);
+        const endX = radius + radius * Math.cos(angle);
+        const endY = radius + radius * Math.sin(angle);
+
+        const longArc = angle > Math.PI ? 1 : 0;
+
+        return `
+        M ${startX} ${startY}
+        A ${radius} ${radius} 90 ${longArc} 1 ${endX} ${endY}
+        `;
+    }
+
     return (
         <div id='allTimeStats'>
-            <h2 id='breakdownTitle'>Won in _ guesses:</h2>
+            <h2 id='breakdownTitle'>Games won in _ guesses:</h2>
             <ol id='breakdown'>
                 {breakdown}
             </ol>
             <h2 id='winrateTitle'>Win Rate</h2>
-            <div id='winRate' style={{'--winrate': `${rate * 3.6}deg`}}>{rate}%</div>
+            <figure id='winRate'>
+                <svg
+                    width='100%' 
+                    height='100%' 
+                    viewBox='0 0 100 100'
+                    xmlns='http://www.w3.org/2000/svg'>
+                    <circle id='lossArc'
+                        cx={50} cy={50} r={50}>
+                    </circle>
+                    <path id='winArc'
+                        d={arcFromRate(rate)}>
+                    </path>
+                </svg>
+                <figcaption style={{'--winrate': `${rate * 3.6}deg`}}>{rate}%</figcaption>
+            </figure>
         </div>
     );
 };
