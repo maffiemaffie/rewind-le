d8116fdbc379f9146ac8cb7cdaed86687935462e
Elia Cohen <mrc6113@rit.edu>
2023-12-10 15:20:49 -0500

    settings code

--- client/settings.jsx
+++ client/settings.jsx
@@ -1,9 +1,80 @@
 const helper = require('./helper.js');
 const React = require('react');
 const ReactDOM = require('react-dom');
+const { EnterUsernameWindow } = require('./linkAccount.jsx');
+
+const AccountWindow = (props) => {
+  return (
+    <div id='accountWindow'>
+      <h2>Acconunt</h2>
+      <form id='updateUsername'>
+        <fieldset className='textField' disabled>
+          <legend className='hidden'>Username:</legend>
+          <label htmlFor='usernameField'>Username</label>
+          <input type='text' id='usernameField' name='username'/>
+          <input type='submit' id='submitChangeUsername' value='Save Changes'/>
+        </fieldset>
+        <input type='button' className='editButton' value='Edit' disabled/>
+      </form>
+      <form id='updatePassword'>
+        <fieldset className='textField' disabled>
+          <legend className='hidden'>Password</legend>
+          <label htmlFor='passwordField'>Password</label>
+          <input type='password' id='passwordField' name='password'/>
+          <label htmlFor='oldPasswordField'>Old Password</label>
+          <input type='password' id='oldPasswordField' name='oldPassword'/>
+          <input type='submit' id='submitChangePassword' value='Save Changes'/>
+        </fieldset>
+        <input type='button' className='editButton' value='Edit'/>
+      </form>
+    </div>
+  )
+}
+
+const PremiumWindow = (props) => {
+  return (
+    <div id='premiumWindow'>
+      <h2>Get Premium</h2>
+      <button id='buyPremium'>Buy</button>
+    </div>
+  )
+}
+
+const LastFmWindow = (props) => {
+  return (
+    <div id='content'>
+      <EnterUsernameWindow/>
+    </div>
+  )
+}
 
 init = () => {
+  document.getElementById('lastFmTab').addEventListener('click', () => {
+    helper.sendPost('/connectLastFm/removeAccount', {});
+    ReactDOM.render(
+      <LastFmWindow/>,
+      document.getElementById('activePage')
+    );
+  });
 
+  document.getElementById('accountTab').addEventListener('click', () => {
+    ReactDOM.render(
+      <AccountWindow/>,
+      document.getElementById('activePage')
+    );
+  });
+
+  document.getElementById('premiumTab').addEventListener('click', () => {
+    ReactDOM.render(
+      <PremiumWindow/>,
+      document.getElementById('activePage')
+    );
+  });
+
+  ReactDOM.render(
+    <AccountWindow/>,
+    document.getElementById('activePage')
+  );
 }
 
 window.onload = init;
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,4 +1,5 @@
 module.exports.Account = require('./Account.js');
 module.exports.Domo = require('./Domo.js');
 module.exports.Game = require('./Game.js');
+module.exports.Settings = require('./Settings.js');
 module.exports.LastFm = require('./LastFm.js');
