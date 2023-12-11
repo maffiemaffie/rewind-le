e7002b6e71d8c8740a94809c93e9e73bb6b612f6
Elia Cohen <mrc6113@rit.edu>
2023-12-11 16:07:24 -0500

    404 page

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,3 +2,4 @@ module.exports.Account = require('./Account.js');
 module.exports.Game = require('./Game.js');
 module.exports.Settings = require('./Settings.js');
 module.exports.LastFm = require('./LastFm.js');
+module.exports.NotFound = require('./NotFound.js');
--- server/router.js
+++ server/router.js
@@ -33,6 +33,7 @@ const router = (app) => {
   app.get('/logout', mid.requiresLogin, controllers.Account.logout);
 
   app.get('/', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
+  app.get('/*', controllers.NotFound);
 };
 
 module.exports = router;
